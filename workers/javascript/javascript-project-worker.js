40b662b105062bd8d9f1e46194f44f26993620e5
obinnanwachukwu1 <me@obinnanwachukwu.com>
2026-07-05 15:12:01 -0400

    release: 0.9.9

--- workers/javascript/javascript-project-worker.js
+++ workers/javascript/javascript-project-worker.js
@@ -3716,7 +3716,7 @@ function unzipSync(data, opts) {
 // packages/harness-javascript/package.json
 var package_default = {
   name: "@tracecode/harness-javascript",
-  version: "0.9.8",
+  version: "0.9.9",
   description: "JavaScript and TypeScript runtime helpers and browser worker assets for TraceCode harness.",
   license: "AGPL-3.0-only",
   homepage: "https://tracecode.app",
